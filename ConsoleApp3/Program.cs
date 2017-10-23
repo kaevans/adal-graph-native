0eaf00acb5ac7370bb34ba900dd85acc35b0886a
Kirk Evans <kirke@microsoft.com>
2017-10-23 11:31:18 -0500

    Updated ReadMe, added code

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Graph;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+var graphClient = new GraphServiceClient(
+    "https://graph.microsoft.com/v1.0",
+    new DelegateAuthenticationProvider(
+        async (requestMessage) =>
+        {
+                                   
+            var ctx = new AuthenticationContext("https://login.microsoftonline.com/microsoft.onmicrosoft.com");
+            var authResult = await ctx.AcquireTokenAsync(
+                "https://graph.microsoft.com/", 
+                "342ccd8e-387c-4369-81b1-813e3a24b5f1", 
+                new Uri("https://localhost"), 
+                new PlatformParameters(PromptBehavior.Auto));
+
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", authResult.AccessToken);
+        }));
+var me = graphClient.Me.Request().GetAsync().GetAwaiter().GetResult();
+
+Console.WriteLine(me.Surname);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # adal-graph-native
 Calling Microsoft Graph with ADAL library from a native client
+
+## Pre-Requisites
+This application requires using a work or school account with Azure AD. It does not support using a personal Microsoft Account or B2C.
+
+## Register in Azure Portal
+Go to the Azure Portal, click on **App Registrations**, and choose **New application registration**.  Provide a name, choose **Native Application** as the type, and give a reply URL of `https://localhost`. 
+
+Once created, click the new application registration and choose **Required permissions**. Click the **Add** button and choose the **Microsoft Graph** API. Under permissions, choose **View users' basic profile**, **View users' email address**, and **Access user's data anytime**. 
+
+(./images/permissions.png)[]
+
+##Edit code
+In Program.cs, replace the client ID guid with the GUID from your application, and replace the name of your tenant in the authority parameter. Run the sample, you should now be able to acquire a token, the code will query the Graph API, and your last name will be displayed.
